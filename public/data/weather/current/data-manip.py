c2e21bf7fa9ee14bb25f77ccf0187e4375c95f88
jeffs5 <jeffs5s@gmail.com>
2014-12-03 00:05:34 -0800

    Model layer and difference working

--- public/data/weather/current/data-manip.py
+++ public/data/weather/current/data-manip.py
@@ -2,17 +2,19 @@ import json
 from pprint import pprint
 from json import encoder
 
-json_data=open('current-wind_model-surface-level-gfs-1.0.json')
+json_data=open('current-wind_actual-surface-level-gfs-1.0.json')
 datalist = []
 encoder.FLOAT_REPR = lambda o: format(o, '.2f')
 
 data = json.load(json_data)
 for i in data:
 	data = i['data']
-	for point in data:
-		# change the zero and the operation to whatever you please
-		datalist.append(point * -1)
-	# pprint(datalist)
+
+print (len(data))
+for point in data:
+	# change the zero and the operation to whatever you please
+	datalist.append(point * 2)
+# pprint(datalist)
 
 with open('current-wind_model-surface-level-gfs-1.0.json', 'r+') as f:
     data = json.load(f)
@@ -21,6 +23,6 @@ with open('current-wind_model-surface-level-gfs-1.0.json', 'r+') as f:
 		i['data'] = datalist
     f.seek(0)        # <--- should reset file position to the beginning.
     json.dump(data, f, separators = (',', ':'))
-    json.dump(data,json_data, 'data', separators = (',', ':'))
+    #json.dump(data,json_data, 'data', separators = (',', ':'))
 
 json_data.close()
--- public/data/weather/current/data-manip2.py
+++ public/data/weather/current/data-manip2.py
@@ -3,18 +3,26 @@ from pprint import pprint
 from json import encoder
 
 json_data=open('current-wind_model-surface-level-gfs-1.0.json')
+json_data2=open('current-wind_actual-surface-level-gfs-1.0.json')
 datalist = []
 encoder.FLOAT_REPR = lambda o: format(o, '.2f')
 
 data = json.load(json_data)
+data2 = json.load(json_data2)
 for i in data:
 	data = i['data']
-	for point in data:
-		# change the zero and the operation to whatever you please
-		datalist.append(point * -1)
-	# pprint(datalist)
+for i in data2:
+	data2 = i['data']
 
-with open('current-wind_model-surface-level-gfs-1.0.json', 'r+') as f:
+print (len(data), len(data2))
+for point in range(0,len(data)):
+	# model minus actual
+	#print (point)
+	datalist.append(data[point] - data2[point])
+	# print(data[point], data2[point])
+# pprint(datalist)
+
+with open('current-wind_difference-surface-level-gfs-1.0.json', 'r+') as f:
     data = json.load(f)
     
     for i in data:
